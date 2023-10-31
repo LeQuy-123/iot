b30d7fb4acd72d4945be3dcbacb3f0292f53cba3
quy.le <>
2023-10-31 21:08:45 +0700

    update

--- iot_app/lib/firebase_messaging.dart
+++ iot_app/lib/firebase_messaging.dart
@@ -5,7 +5,7 @@
 import 'dart:async';
 // import 'dart:convert';
 import 'dart:developer' as developer;
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
